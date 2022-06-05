7786f8520eb40f748b1dc7dde275d2b2a11db307
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-06-06 00:28:52 +0300

    Adds the javascrip code for submit functionality

--- portfollio.js
+++ portfollio.js
@@ -3,3 +3,66 @@
 const scroll = new SmoothScroll('a[href*="#"]', {
     speed: 1000
 });
+
+// Adding event listener
+document.querySelector('userInputForm');
+document.addEventListener("submit", function(e){
+   // getting Input data into global varaibles
+
+const name = document.getElementById("name").value;
+const email = document.getElementById("email").value;
+const phoneNumber = document.getElementById("phone").value;
+const message =document.getElementById("message").value;
+
+// An Array of objects to store our user input data
+const clientList= [];
+
+// Submit function onsubmit event
+ function submit(name,email, phoneNumber,message) {
+     if (name !==" " && email !== "" && phoneNumber !== " " && message !== " ") {
+         const client = {
+             name : name,
+             email : email,
+             phoneNumber : phoneNumber,
+             message : message,
+         }
+         clientList.push(client);
+         // storing our array in local storage
+         localStorage.setItem('clientList', JSON.stringify(clientList));
+         document.querySelector('form').reset();
+         alert("Form submitted susccessfully.");
+     } else {
+         alert("Kindly Fill all fields correctly");
+     }
+ }
+ 
+})
+
+// getting Input data into global varaibles
+
+/*const name = document.getElementById("name").value;
+const email = document.getElementById("email").value;
+const phoneNumber = document.getElementById("phone").value;
+const message =document.getElementById("message").value;
+
+// An Array of objects to store our user input data
+const clientList= [];
+
+// Submit function onsubmit event
+ function submit(name,email, phoneNumber,message) {
+     if (name !==" " && email !== "" && phoneNumber !== " " && message !== " ") {
+         const client = {
+             name : name,
+             email : email,
+             phoneNumber : phoneNumber,
+             message : message,
+         }
+         clientList.push(client);
+         // storing our array in local storage
+         localStorage.setItem('clientList', JSON.stringify(clientList));
+         document.querySelector('form').reset();
+         alert("Form submitted susccessfully.");
+     } else {
+         alert("Kindly Fill all fields correctly");
+     }
+ }*/
