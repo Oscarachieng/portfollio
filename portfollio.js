0f317bb8e04373192d0f57ecd5c92c4398613bc1
Oscarachieng <95440374+Oscarachieng@users.noreply.github.com>
2022-06-07 08:52:10 +0300

    Add files via upload
    Updates button on both HTML and JavaScript to add functionality

--- portfollio.js
+++ portfollio.js
@@ -1,68 +1,56 @@
 // Adds Smooth Scroll script
 
 const scroll = new SmoothScroll('a[href*="#"]', {
-    speed: 1000
+  speed: 1000,
 });
 
 // Adding event listener
-document.querySelector('userInputForm');
-document.addEventListener("submit", function(e){
-   // getting Input data into global varaibles
+document.querySelector("#submit");
+document.addEventListener("submit", function (e) {
 
-const name = document.getElementById("name").value;
-const email = document.getElementById("email").value;
-const phoneNumber = document.getElementById("phone").value;
-const message =document.getElementById("message").value;
+  e.preventDefault();
+  console.log(e)
+
 
 // An Array of objects to store our user input data
-const clientList= [];
+  const clientList = [];
+
+  // getting Input data into global varaibles
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const phoneNumber = document.getElementById("phone").value;
+  const message = document.getElementById("message").value;
+
+  if (name !== " " && email !== "" && phoneNumber !== " " && message !== " ") {
+    const client = {
+      name: name,
+      email: email,
+      phoneNumber: phoneNumber,
+      message: message,
+    };
+    clientList.push(client);
+
+      // storing our array in local storage
+      window.localStorage.setItem("clientList", JSON.stringify(clientList));
+      document.querySelector("form").reset();
+      alert("Message submitted susccessfully, click OK to continue. Thanks");
+    } else {
+      alert("Kindly Fill all fields correctly");
+    }
+
+});
+
+
+
+
+
+
 
 // Submit function onsubmit event
- function submit(name,email, phoneNumber,message) {
-     if (name !==" " && email !== "" && phoneNumber !== " " && message !== " ") {
-         const client = {
-             name : name,
-             email : email,
-             phoneNumber : phoneNumber,
-             message : message,
-         }
-         clientList.push(client);
-         // storing our array in local storage
-         localStorage.setItem('clientList', JSON.stringify(clientList));
-         document.querySelector('form').reset();
-         alert("Form submitted susccessfully.");
-     } else {
-         alert("Kindly Fill all fields correctly");
-     }
- }
+//function submit(e) {
  
-})
-
-// getting Input data into global varaibles
-
-/*const name = document.getElementById("name").value;
-const email = document.getElementById("email").value;
-const phoneNumber = document.getElementById("phone").value;
-const message =document.getElementById("message").value;
-
-// An Array of objects to store our user input data
-const clientList= [];
-
-// Submit function onsubmit event
- function submit(name,email, phoneNumber,message) {
-     if (name !==" " && email !== "" && phoneNumber !== " " && message !== " ") {
-         const client = {
-             name : name,
-             email : email,
-             phoneNumber : phoneNumber,
-             message : message,
-         }
-         clientList.push(client);
-         // storing our array in local storage
-         localStorage.setItem('clientList', JSON.stringify(clientList));
-         document.querySelector('form').reset();
-         alert("Form submitted susccessfully.");
-     } else {
-         alert("Kindly Fill all fields correctly");
-     }
- }*/
+  // getting Input data into global varaibles
+ 
+ 
+  
+//}
